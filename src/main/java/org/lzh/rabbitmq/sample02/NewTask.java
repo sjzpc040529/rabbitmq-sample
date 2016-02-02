a4195a2384cfb9d3f40023f8b84e5f736c5ac695
zhaohua.li <zhaohua.li@tqmall.com>
2016-02-02 10:22:43 +0800

    调整了java文件的位置

--- src/main/java/org/lzh/rabbitmq/sample02/NewTask.java
+++ src/main/java/org/lzh/rabbitmq/sample02/NewTask.java
@@ -1,11 +1,11 @@
 package org.lzh.rabbitmq.sample02;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 /**
  *  生产者 produces  发送端
