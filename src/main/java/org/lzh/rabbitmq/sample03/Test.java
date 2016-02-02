c21b8bc55567b849a12fca52a7fc42c0ff97a2d5
zhaohua.li <zhaohua.li@tqmall.com>
2016-02-02 16:35:32 +0800

    添加测试代码

---
+++ src/main/java/org/lzh/rabbitmq/sample03/Test.java
@@ -0,0 +1,13 @@
+package org.lzh.rabbitmq.sample03;
+
+/**
+ * @Description:
+ * @author: lizhaohua
+ * @date: 16/2/2 下午4:34
+ * @version: V1.0
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println();
+    }
+}
